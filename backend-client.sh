4f4ad3042376cc4db04529c983135f009b7596d5
Jan Oppolzer <jan@oppolzer.cz>
2019-02-07 16:12:17 +0100

    Add backend-client.sh
    This script is intended to run at a developer's machine to get access to
    the Git repository hosting SAML metadata.

---
+++ backend-client.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+if [ -z ${1} ]; then
+    echo -e "\nMissing Git repository where SAML metadata is stored.\n"
+    exit 1
+fi
+
+DIR_BASE=~/tmp/metaman-client
+DIR_GIT_REMOTE=${1}
+DIR_GIT_REPO=${DIR_BASE}/metaman-git
+
+mkdir -p ${DIR_BASE}
+cd ${DIR_BASE}
+
+# Using `new` branch, because `master` is still being used by Jagger
+git clone -b new git@github.com:CESNET/saml-validator
+
+git clone git@github.com:CESNET/metaman-helpers
+
+git clone ${DIR_GIT_REMOTE} ${DIR_GIT_REPO}
+
+cp ${DIR_BASE}/metaman-helpers/{pre-commit,commit-msg} ${DIR_GIT_REPO}/.git/hooks
+
+cd ${DIR_BASE}/metaman-helpers
+cp config.example config
+sed -i '' -e "s%SAML_VALIDATOR=/path/to/saml-validator/%SAML_VALIDATOR=${DIR_BASE}/saml-validator/%" config
+
