1db54ca12288d22633eac2a0148180eb245945ce
Jan Oppolzer <jan@oppolzer.cz>
2019-02-07 16:39:01 +0100

    Improve backend-server.sh
    Git repositories are not cloned using SSH anymore, but using HTTPS.
    Helpers are symlinked instead of copied.

--- backend-server.sh
+++ backend-server.sh
@@ -10,20 +10,20 @@ cd ${DIR_BASE}
 git init --bare ${DIR_GIT_BARE}
 
 # Using `new` branch, because `master` is still being used by Jagger
-git clone -b new git@github.com:CESNET/saml-validator
+git clone -b new https://github.com/CESNET/saml-validator
 
-git clone git@github.com:CESNET/metaman-helpers
+git clone https://github.com/CESNET/metaman-helpers
 
 git clone ${DIR_GIT_BARE} ${DIR_GIT_REPO}
 cd ${DIR_GIT_REPO}
-cp ${DIR_BASE}/metaman-helpers/gitignore .gitignore
+ln -s ${DIR_BASE}/metaman-helpers/gitignore .gitignore
 git add .gitignore
 git commit -m "${USER}: .gitignore (add)"
 git push
 git config commit.template .commit-message
 
-cp ${DIR_BASE}/metaman-helpers/update ${DIR_GIT_BARE}/hooks
-cp ${DIR_BASE}/metaman-helpers/{pre-commit,commit-msg} ${DIR_GIT_REPO}/.git/hooks
+ln -s ${DIR_BASE}/metaman-helpers/update ${DIR_GIT_BARE}/hooks
+ln -s ${DIR_BASE}/metaman-helpers/{pre-commit,commit-msg} ${DIR_GIT_REPO}/.git/hooks
 
 cd ${DIR_BASE}/metaman-helpers
 cp config.example config
